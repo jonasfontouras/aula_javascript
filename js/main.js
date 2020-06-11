086021a112389f4fac8c3d386ca7b921bd5dbc4d
jonasfontouras <jonasfontouras@gmail.com>
2020-06-11 20:27:04 -0300

    Introdução ao javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Jonas";
+var idade = 19;
+var frase = "Harvard é a melhor faculdade do mundo.";
+
+alert(nome + " tem " + idade + " anos.");
+
+console.log(frase);
+
+console.log(frase.replace("Harvard", "AMF").toUpperCase());
+
+alert(frase.replace("Harvard", "AMF"));
