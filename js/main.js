ba309b213c044fb536ed80be01c65578c145ca64
jonasfontouras <jonasfontouras@gmail.com>
2020-06-17 15:41:31 -0300

    Curso de Javascript - Manipulando elemntos da página

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-var nome = "Jonas";
-var idade = 19;
-var frase = "Harvard é a melhor faculdade do mundo.";
+var turismo_recanto = ["Hotel Capo Zorial", "Hotel Beira-Rio", "Dipaolo"];
+var curso = [{Fculdade: "AMF"}, {curso: "Sistemas de Informação"}, {curso: "Ontopsicologia"}, {curso: "Administração"}, {curso: "Direito"}, {curso: "Pedagogia"}];
+turismo_recanto.push("Domus Mea");
+turismo_recanto.pop("Dipaolo");
 
-alert(nome + " tem " + idade + " anos.");
+console.log(curso);
 
-console.log(frase);
+console.log(turismo_recanto.toString());
 
-console.log(frase.replace("Harvard", "AMF").toUpperCase());
+console.log(turismo_recanto.join(" - "));
 
-alert(frase.replace("Harvard", "AMF"));
+
+
+
+var data = new Date();
+alert(data)
+var nome = prompt("Digite seu nome: ");
+var idade = prompt(nome + ", digite sua idade: ");
+
+if (idade <= 18){
+    alert(nome + ", você tem " + idade + " anos e é menor de idade.");
+}
+else{
+    alert(nome + ", você tem " + idade + " anos e é maior de idade.");
+}
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(10, 5));
+//console.log(frase);
+
+//console.log(frase.replace("Harvard", "AMF").toUpperCase());
+
+//alert(frase.replace("Harvard", "AMF"));
